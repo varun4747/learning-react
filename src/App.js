d3639d299eed42c5e952076c66935034b150376c
varun4747 <varunpapanaboina4747@gmail.com>
2025-09-13 16:21:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h3>React App Development</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
